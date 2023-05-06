3566071b172e0e913a0c5e132d882fe1c3345693
SajidAbad <132222344+SajidAbad@users.noreply.github.com>
2023-05-06 17:38:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ streamlit.dataframe(fruits_to_show)
 # New Section
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/+"kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 # streamlit.text(fruityvice_response.json()) #just write the data to the screem
 
 # retrive data structure and data
